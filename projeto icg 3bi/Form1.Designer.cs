1f179de17781024c489463aedfa823d5a9b2da29
Yan Ueno <54038919+YanUeno@users.noreply.github.com>
2021-09-14 21:55:34 -0300

    Add files via upload

---
+++ projeto icg 3bi/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace projeto_icg_3bi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureC = new System.Windows.Forms.PictureBox();
+            this.pictureB = new System.Windows.Forms.PictureBox();
+            this.pictureA = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureC
+            // 
+            this.pictureC.Location = new System.Drawing.Point(920, 33);
+            this.pictureC.Name = "pictureC";
+            this.pictureC.Size = new System.Drawing.Size(323, 278);
+            this.pictureC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureC.TabIndex = 6;
+            this.pictureC.TabStop = false;
+            // 
+            // pictureB
+            // 
+            this.pictureB.Image = ((System.Drawing.Image)(resources.GetObject("pictureB.Image")));
+            this.pictureB.Location = new System.Drawing.Point(486, 33);
+            this.pictureB.Name = "pictureB";
+            this.pictureB.Size = new System.Drawing.Size(323, 278);
+            this.pictureB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureB.TabIndex = 5;
+            this.pictureB.TabStop = false;
+            // 
+            // pictureA
+            // 
+            this.pictureA.Image = ((System.Drawing.Image)(resources.GetObject("pictureA.Image")));
+            this.pictureA.Location = new System.Drawing.Point(60, 33);
+            this.pictureA.Name = "pictureA";
+            this.pictureA.Size = new System.Drawing.Size(323, 278);
+            this.pictureA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureA.TabIndex = 4;
+            this.pictureA.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1375, 579);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureC);
+            this.Controls.Add(this.pictureB);
+            this.Controls.Add(this.pictureA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureA)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureC;
+        private System.Windows.Forms.PictureBox pictureB;
+        private System.Windows.Forms.PictureBox pictureA;
+    }
+}
+
---
+++ projeto icg 3bi/Form1.cs
@@ -0,0 +1,126 @@
+﻿/* *******************************************************************
+ * Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ * Curso Técnico em Informática - Data de Entrega: 30/09/2020
+ * Autores do Projeto: Yan massahiro ueno
+ * Turma: 3f
+ * Atividade Proposta em aula
+ * Observação: <colocar se houver>
+ * 
+ * Nome_prog.cs
+ * ******************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace projeto_icg_3bi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap pato;
+        Bitmap barco;
+        Bitmap fotopronta;
+        private void junta_imagem()
+        {
+            Bitmap foto = pato;
+            Bitmap cenario = barco;
+  
+            int larg = pato.Width -1;
+            int alt = pato.Width - 1;
+            for (int l = 1; l <= larg-1; l++)
+            {
+                for (int a = 1; a <= alt-1; a++)
+                {
+                    int r = pato.GetPixel(l,a).R;
+                    int g = pato.GetPixel(l, a).G;
+                    int b = pato.GetPixel(l, a).B;
+                    if (r== 127 && g == 127 && b == 127)
+                    {
+                        //
+                    }
+                    else
+                        //MessageBox.Show("asdasd");
+                        cenario.SetPixel(l + 260,a ,Color.FromArgb(r,g,b));
+                    }
+                }
+
+            pictureA.Image = cenario;
+            fotopronta = new Bitmap(cenario);
+            fotopronta.Save("fotoColorida.bmp");
+        }
+
+        Bitmap PB;
+        private void convertegrayscale()
+        {
+            int larg = fotopronta.Width;
+            int alt = fotopronta.Height;
+            PB = new Bitmap(fotopronta);
+            for (int l = 0; l <= larg - 1; l++)
+            {
+                for (int a = 0; a <= alt - 1; a++)
+                {
+                    int r = fotopronta.GetPixel(l, a).R;
+                    int g = fotopronta.GetPixel(l, a).G;
+                    int b = fotopronta.GetPixel(l, a).B;
+                    int cinza = (int)((r * 0.30) + (g * 0.59) + (b * 0.11));
+                    PB.SetPixel(l, a, Color.FromArgb(cinza, cinza, cinza));
+
+                }
+            }
+            pictureB.Image = PB;
+            PB.Save("fotoPretoBranco.bmp");
+        }
+        Bitmap binario;
+        private void converterBinario()
+        {
+            int larg = PB.Width;
+            int alt = PB.Height;
+            binario = new Bitmap(PB);
+            for (int l = 1; l <= larg - 1; l++)
+            {
+                for (int a = 1; a <= alt - 1; a++)
+                {
+                    int r = binario.GetPixel(l, a).R;
+                    int g = binario.GetPixel(l, a).G;
+                    int b = binario.GetPixel(l, a).B;
+                    int cinza = (int)((r * 0.30) + (g * 0.59) + (b * 0.11));
+                    if (cinza < 127)
+                    {
+                        binario.SetPixel(l, a, Color.FromArgb(0, 0, 0));
+                    }
+                    else
+                    {
+                        binario.SetPixel(l, a, Color.FromArgb(255, 255, 255));
+                    }
+
+                }
+            }
+            pictureC.Image = binario;
+            binario.Save("Binario.bmp");
+        }      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            junta_imagem();
+            convertegrayscale();
+            converterBinario();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pato = new Bitmap(pictureB.Image);
+            barco = new Bitmap(pictureA.Image);
+        }  
+             
+    }
+}
